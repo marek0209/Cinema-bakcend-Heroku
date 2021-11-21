c49c63c3a0ceffea8288ac4eb94d9e22cc28e6a1
Marek Urbaniec <marek@aspengrovestudios.com>
2021-11-21 21:07:15 +0100

    Add potential fix

--- src/index.js
+++ src/index.js
@@ -42,3 +42,16 @@ app.use("/api/seanses", seanses());
 app.listen(config.server.port, () => {
   console.log(`API server works at port:` + config.server.port);
 });
+
+//For avoidong Heroku $PORT error
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
